9a8f5609149baee13d3253d0b83c88948c1be4c4
h2danilo <h2danilo@hotmail.com>
2025-08-20 18:53:33 -0300

    feat(): aula 14/08

---
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -0,0 +1,23 @@
+package com.programacao.web.fatec.api_fatec.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+    
+    @GetMapping("/testeCliente1") //-> /api/clientes/testeCliente1
+    public String testeCliente() {
+        return "Teste Client";
+    }
+
+    @GetMapping("/testeCliente2/{nome}") //-> /api/clientes/testeCliente2/
+    public String testeCliente2(@PathVariable String nome) {
+        return nome;
+    }
+}
