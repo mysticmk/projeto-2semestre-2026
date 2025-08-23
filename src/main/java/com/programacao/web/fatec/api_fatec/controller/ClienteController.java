174959e57824848e082fe26e220358ad49a326d7
h2danilo <h2danilo@hotmail.com>
2025-08-23 10:52:56 -0300

    feat(): listarClientes buscando Banco dados

--- src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -55,7 +55,7 @@ public class ClienteController {
 
     @GetMapping("/listarClientes")
     public List<Cliente> listarClientes() {
-        return listaDeCliente;
+        return clienteRepository.findAll();
     }
     
 
